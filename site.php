980c79e6d482d24d00841e1851424fc3c65799d2
Hammad <hmdbupt@yahoo.com>
2020-07-11 00:37:15 +0500

    Hello World

---
+++ site.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello World In PHP</title>
+</head>
+<body>
+    <?php 
+        // echo prints out to the html
+        echo "<h1>Hello, PHP!</h1>";
+    ?>
+</body>
+</html>
